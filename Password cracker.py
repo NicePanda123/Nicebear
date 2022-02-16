a85a155d595a1ea12d9a42b1b509aa86e861d685
NicePanda123 <99769802+NicePanda123@users.noreply.github.com>
2022-02-15 20:03:42 -0500

    Create Password cracker.py

---
+++ Password cracker.py
@@ -0,0 +1,17 @@
+import random
+import pyautogui
+
+chars = "abcdefghijklmnopqrstuvwxyz1234567890"
+chars_list = list(chars)
+
+password = pyautogui.password("Enter A Password: ")
+guess_password = ""
+
+while(guess_password != password):
+	guess_password = random.choices(chars_list, k=len(password))
+	print("<==========="+ str(guess_password) + "===========>")
+
+	if(guess_password == list(password)):
+		print("Your Password Is: " + "".join(guess_password))
+		input()
+		break
